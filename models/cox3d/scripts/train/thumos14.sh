ea7d2a2f61e0126e460d319a356bc42673123cb7
LukasHedegaard <lh@eng.au.dk>
2021-09-24 13:15:47 +0000

    Update thumos14 train script

--- models/cox3d/scripts/train/thumos14.sh
+++ models/cox3d/scripts/train/thumos14.sh
@@ -6,7 +6,7 @@ fi
 
 PROJECT=models/cox3d
 DATASET=thumos14
-GPUS=4
+GPUS=3
 DISTRIBUTED_BACKEND=ddp
 PRECISION=32
 
@@ -19,7 +19,7 @@ FRAMES_PER_CLIP=13
 FORWARD_FRAME_DELAY=$(($FRAMES_PER_CLIP + 56))
 NUM_FORWARD_FRAMES=$((128 - $FORWARD_FRAME_DELAY))
 BATCH_SIZE=16
-LR=2.0 # $BATCH_SIZE * $GPUS * $NUM_FORWARD_FRAMES * OLD_LR / OLD_BS == 21
+LR=0.05 # $BATCH_SIZE * $GPUS * $NUM_FORWARD_FRAMES * OLD_LR / OLD_BS == 21
 
 python $PROJECT/main.py \
     --id "CoX3D_${MODEL}_${DATASET}_${FRAMES_PER_CLIP}_frames" \
@@ -31,17 +31,19 @@ python $PROJECT/main.py \
     --batch_size $BATCH_SIZE \
     --benchmark True \
     --train \
-    --max_epochs 1 \
+    --max_epochs 30 \
     --logging_backend wandb \
     --num_workers 8 \
-    --frames_per_clip $FRAMES_PER_CLIP \
     --co3d_forward_mode init_frame \
     --co3d_forward_frame_delay $FORWARD_FRAME_DELAY  \
     --co3d_num_forward_frames $NUM_FORWARD_FRAMES \
+    --step_between_clips $NUM_FORWARD_FRAMES \
+    --frames_per_clip $FRAMES_PER_CLIP \
     --precision $PRECISION \
-    --distributed_backend $DISTRIBUTED_BACKEND \
-    --optimization_metric top1acc \
+    --optimization_metric mAP \
     --unfreeze_from_epoch 0 \
     --learning_rate $LR \
     --num_sanity_val_steps 0 \
+    --unfreeze_layer_step 2 \
+    --distributed_backend $DISTRIBUTED_BACKEND \
 
