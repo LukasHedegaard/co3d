716ce897400d9fa48db01c900e0c3f6bd64d1d0f
LukasHedegaard <lh@eng.au.dk>
2021-10-08 08:50:08 +0000

    Update calibrated_average_precision metric

--- metrics/calibrated_average_precision.py
+++ metrics/calibrated_average_precision.py
@@ -26,8 +26,8 @@ def mean_calibrated_average_precision(
     return tuple of
         mAP (float): mean average precision
         mcAP (float): mean calibrated average precision
-        classwise_AP (List[float]): class-wise average precision
-        classwise_cAP (List[float]): class-wise calibrated average precision
+        classwise_AP (Dict[int, float]): class-wise average precision
+        classwise_cAP (Dict[int, float]): class-wise calibrated average precision
     """
     N, C = preds.shape
 
@@ -35,19 +35,20 @@ def mean_calibrated_average_precision(
     if len(targets.shape) == 1:
         targets = torch.nn.functional.one_hot(targets, C)
 
-    if skip_classes:
-        cids = torch.arange(C)
-        sel = torch.tensor([True for _ in range(C)])
-        for c in skip_classes:
-            sel.logical_and_(cids != c)
-        preds = preds[:, sel]
-        C = preds.shape[1]
+    # Select subset of classes
+    cids = torch.arange(C)
+    sel = torch.tensor([True for _ in range(C)])
+    for c in skip_classes:
+        sel.logical_and_(cids != c)
+    preds = preds[:, sel]
+    targets = targets[:, sel]
+    C = preds.shape[1]
 
     empty = {"skip": float("nan"), "neg": 0.0, "pos": 1.0}[empty_target_action]
     classwise_AP = [empty for _ in range(C)]
     classwise_cAP = [empty for _ in range(C)]
 
-    for i in range(0, C):
+    for i in range(C):
         cls_preds = preds[:, i]
         cls_targets = targets[:, i]
 
@@ -73,18 +74,58 @@ def mean_calibrated_average_precision(
         classwise_AP[i] = cls_AP
         classwise_cAP[i] = cls_cAP
 
+    classwise_AP_dict = {int(k): v for k, v in zip(list(cids[sel]), classwise_AP)}
+    classwise_cAP_dict = {int(k): v for k, v in zip(list(cids[sel]), classwise_cAP)}
+
     if empty_target_action == "skip":
         classwise_AP = [c for c in classwise_AP if not math.isnan(c)]
         classwise_cAP = [c for c in classwise_cAP if not math.isnan(c)]
 
     if len(classwise_AP) > 0:
         mAP = sum(classwise_AP) / len(classwise_AP)
-        cmAP = sum(classwise_cAP) / len(classwise_cAP)
+        mcAP = sum(classwise_cAP) / len(classwise_cAP)
     else:
         mAP = empty
-        cmAP = empty
+        mcAP = empty
 
-    return mAP, cmAP, classwise_AP, classwise_cAP
+    return mAP, mcAP, classwise_AP_dict, classwise_cAP_dict
+
+
+def _formatted_mean_calibrated_average_precision(
+    preds: Tensor,
+    targets: Tensor,
+    skip_classes: Sequence[int] = [],
+    empty_target_action="skip",
+    class_names=[],
+):
+    (
+        mAP,
+        mcAP,
+        classwise_AP_dict,
+        classwise_cAP_dict,
+    ) = mean_calibrated_average_precision(
+        preds,
+        targets,
+        skip_classes,
+        empty_target_action,
+    )
+
+    if len(class_names):
+
+        def name(k):
+            return f"{class_names[k]} ({k})"
+
+    else:
+
+        def name(k):
+            return k
+
+    return {
+        "mAP": torch.tensor(mAP),
+        "mcAP": torch.tensor(mcAP),
+        **{f"AP/{name(k)}": torch.tensor(v) for k, v in classwise_AP_dict.items()},
+        **{f"cAP/{name(k)}": torch.tensor(v) for k, v in classwise_cAP_dict.items()},
+    }
 
 
 class CalibratedMeanAveragePrecisionMetric(MetricMixin):
@@ -125,15 +166,13 @@ class CalibratedMeanAveragePrecisionMetric(MetricMixin):
     def metrics_step(
         self, preds: Tensor, targets: Tensor, *args, **kwargs
     ) -> MetricDict:
-        mAP, mcAP, _, _ = mean_calibrated_average_precision(
-            preds, targets, self.hparams.mean_average_precision_skip_classes
+        return _formatted_mean_calibrated_average_precision(
+            preds, targets, class_names=getattr(self, "classes", [])
         )
-        return {"mAP": torch.tensor(mAP), "mcAP": torch.tensor(mcAP)}
 
     def metrics_epoch(
         self, preds: Tensor, targets: Tensor, *args, **kwargs
     ) -> MetricDict:
-        mAP, mcAP, _, _ = mean_calibrated_average_precision(
-            preds, targets, self.hparams.mean_average_precision_skip_classes
+        return _formatted_mean_calibrated_average_precision(
+            preds, targets, class_names=getattr(self, "classes", [])
         )
-        return {"mAP": torch.tensor(mAP), "mcAP": torch.tensor(mcAP)}
