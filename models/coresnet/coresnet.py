5ba6aa2ccc40eddf326f389726df00b26301cea7
LukasHedegaard <lh@eng.au.dk>
2021-11-02 09:02:45 +0100

    Add preliminary impl for CoResNet

---
+++ models/coresnet/coresnet.py
@@ -0,0 +1,535 @@
+from collections import OrderedDict
+from typing import Sequence, Tuple
+
+import continual as co
+from torch import nn
+from models.common import CoResStage, init_weights
+
+# Number of blocks for different stages given the model depth.
+_MODEL_STAGE_DEPTH = {50: (3, 4, 6, 3), 101: (3, 4, 23, 3)}
+
+# Basis of temporal kernel sizes for each of the stage.
+_TEMPORAL_KERNEL_BASIS = {
+    "2d": [
+        [[1]],  # conv1 temporal kernel.
+        [[1]],  # res2 temporal kernel.
+        [[1]],  # res3 temporal kernel.
+        [[1]],  # res4 temporal kernel.
+        [[1]],  # res5 temporal kernel.
+    ],
+    "c2d": [
+        [[1]],  # conv1 temporal kernel.
+        [[1]],  # res2 temporal kernel.
+        [[1]],  # res3 temporal kernel.
+        [[1]],  # res4 temporal kernel.
+        [[1]],  # res5 temporal kernel.
+    ],
+    "c2d_nopool": [
+        [[1]],  # conv1 temporal kernel.
+        [[1]],  # res2 temporal kernel.
+        [[1]],  # res3 temporal kernel.
+        [[1]],  # res4 temporal kernel.
+        [[1]],  # res5 temporal kernel.
+    ],
+    "i3d": [
+        [[5]],  # conv1 temporal kernel.
+        [[3]],  # res2 temporal kernel.
+        [[3, 1]],  # res3 temporal kernel.
+        [[3, 1]],  # res4 temporal kernel.
+        [[1, 3]],  # res5 temporal kernel.
+    ],
+    "i3d_nopool": [
+        [[5]],  # conv1 temporal kernel.
+        [[3]],  # res2 temporal kernel.
+        [[3, 1]],  # res3 temporal kernel.
+        [[3, 1]],  # res4 temporal kernel.
+        [[1, 3]],  # res5 temporal kernel.
+    ],
+    "slow": [
+        [[1]],  # conv1 temporal kernel.
+        [[1]],  # res2 temporal kernel.
+        [[1]],  # res3 temporal kernel.
+        [[3]],  # res4 temporal kernel.
+        [[3]],  # res5 temporal kernel.
+    ],
+}
+
+# Size of stride on different res stages.
+_SPATIAL_STRIDES = {False: [1, 2, 2, 2], True: [1, 1, 1, 2]}  # key: enable_detection
+
+# Size of dilation on different res stages.
+_SPATIAL_DILATIONS = {False: [1, 1, 1, 1], True: [1, 2, 2, 1]}  # key: enable_detection
+
+_POOL1 = {
+    "2d": [1, 1, 1],
+    "c2d": [2, 1, 1],
+    "c2d_nopool": [1, 1, 1],
+    "i3d": [2, 1, 1],
+    "i3d_nopool": [1, 1, 1],
+    "slow": [1, 1, 1],
+}
+
+
+def CoResNetBasicStem(
+    dim_in: int,
+    dim_out: int,
+    kernel: int,
+    stride: int,
+    padding: int,
+    inplace_relu=True,
+    eps=1e-5,
+    bn_mmt=0.1,
+    norm_module=nn.BatchNorm3d,
+    temporal_fill: co.PaddingMode = "zeros",
+    *args,
+    **kwargs,
+):
+    """
+    Basic 3D Resnet stem module.
+
+    Args:
+        dim_in (int): the channel dimension of the input. Normally 3 is used
+            for rgb input, and 2 or 3 is used for optical flow input.
+        dim_out (int): the output dimension of the convolution in the stem
+            layer.
+        kernel (list): the kernel size of the convolution in the stem layer.
+            temporal kernel size, height kernel size, width kernel size in
+            order.
+        stride (list): the stride size of the convolution in the stem layer.
+            temporal kernel stride, height kernel size, width kernel size in
+            order.
+        padding (int): the padding size of the convolution in the stem
+            layer, temporal padding size, height padding size, width
+            padding size in order.
+        inplace_relu (bool): calculate the relu on the original input
+            without allocating new memory.
+        eps (float): epsilon for batch norm.
+        bn_mmt (float): momentum for batch norm. Noted that BN momentum in
+            PyTorch = 1 - BN momentum in Caffe2.
+        norm_module (torch.nn.Module): torch.nn.Module for the normalization layer. The
+            default is torch.nn.BatchNorm3d.
+    """
+
+    conv = co.Conv3d(
+        dim_in,
+        dim_out,
+        kernel_size=kernel,
+        stride=stride,
+        padding=padding,
+        bias=False,
+    )
+
+    bn = norm_module(num_features=dim_out, eps=eps, momentum=bn_mmt)
+
+    relu = nn.ReLU(inplace_relu)
+
+    pool_layer = nn.MaxPool3d(
+        kernel_size=[1, 3, 3], stride=[1, 2, 2], padding=[0, 1, 1]
+    )
+
+    # Wrap in sequential to match weight specification
+    return co.Sequential(
+        OrderedDict(
+            [
+                (
+                    "pathway0_stem",
+                    co.Sequential(
+                        OrderedDict(
+                            [
+                                ("conv", conv),
+                                ("bn", bn),
+                                ("relu", relu),
+                                ("pool_layer", pool_layer),
+                            ]
+                        )
+                    ),
+                )
+            ]
+        )
+    )
+
+
+def CoBottleneckTransform(
+    dim_in: int,
+    dim_out: int,
+    temp_kernel_size: int,
+    stride: int,
+    dim_inner: int,
+    num_groups: int,
+    stride_1x1=False,
+    inplace_relu=True,
+    eps=1e-5,
+    bn_mmt=0.1,
+    dilation=1,
+    norm_module=nn.BatchNorm3d,
+    block_idx=0,
+    emporal_window_size: int = 4,
+    temporal_fill: co.PaddingMode = "zeros",
+):
+    """
+    Args:
+        dim_in (int): the channel dimensions of the input.
+        dim_out (int): the channel dimension of the output.
+        temp_kernel_size (int): the temporal kernel sizes of the middle
+            convolution in the bottleneck.
+        stride (int): the stride of the bottleneck.
+        dim_inner (int): the inner dimension of the block.
+        num_groups (int): number of groups for the convolution. num_groups=1
+            is for standard ResNet like networks, and num_groups>1 is for
+            ResNeXt like networks.
+        stride_1x1 (bool): if True, apply stride to 1x1 conv, otherwise
+            apply stride to the 3x3 conv.
+        inplace_relu (bool): if True, calculate the relu on the original
+            input without allocating new memory.
+        eps (float): epsilon for batch norm.
+        bn_mmt (float): momentum for batch norm. Noted that BN momentum in
+            PyTorch = 1 - BN momentum in Caffe2.
+        dilation (int): size of dilation.
+        norm_module (torch.nn.Module): torch.nn.Module for the normalization layer. The
+            default is torch.nn.BatchNorm3d.
+    """
+    (str1x1, str3x3) = (stride, 1) if stride_1x1 else (1, stride)
+
+    a = co.Conv3d(
+        dim_in,
+        dim_inner,
+        kernel_size=(temp_kernel_size, 1, 1),
+        stride=(1, str1x1, str1x1),
+        padding=(int(temp_kernel_size // 2), 0, 0),
+        bias=False,
+    )
+
+    a_bn = co.forward_stepping(
+        norm_module(num_features=dim_inner, eps=eps, momentum=bn_mmt)
+    )
+
+    a_relu = nn.ReLU(inplace=inplace_relu)
+
+    # Tx3x3, BN, ReLU.
+    b = co.Conv3d(
+        dim_inner,
+        dim_inner,
+        kernel_size=(1, 3, 3),
+        stride=(1, str3x3, str3x3),
+        padding=(0, dilation, dilation),
+        groups=num_groups,
+        bias=False,
+        dilation=(1, dilation, dilation),
+        temporal_fill=temporal_fill,
+    )
+
+    b_bn = co.forward_stepping(
+        norm_module(num_features=dim_inner, eps=eps, momentum=bn_mmt)
+    )
+
+    b_relu = nn.ReLU(inplace=inplace_relu)
+
+    # 1x1x1, BN.
+    c = co.Conv3d(
+        dim_inner,
+        dim_out,
+        kernel_size=(1, 1, 1),
+        stride=(1, 1, 1),
+        padding=(0, 0, 0),
+        bias=False,
+    )
+
+    c_bn = co.forward_stepping(
+        norm_module(num_features=dim_out, eps=eps, momentum=bn_mmt)
+    )
+
+    c_bn.transform_final_bn = True
+
+    return co.Sequential(
+        OrderedDict(
+            [
+                ("a", a),
+                ("a_bn", a_bn),
+                ("a_relu", a_relu),
+                ("b", b),
+                ("b_bn", b_bn),
+                ("b_relu", b_relu),
+                ("c", c),
+                ("c_bn", c_bn),
+            ]
+        )
+    )
+
+
+def CoResNetBasicHead(
+    dim_in: int,
+    num_classes: int,
+    pool_size: Tuple[int, int, int],
+    dropout_rate=0.0,
+    act_func="softmax",
+    temporal_window_size: int = 4,
+    temporal_fill: co.PaddingMode = "zeros",
+):
+    """
+    ResNe(X)t 3D head.
+    This layer performs a fully-connected projection during training, when the
+    input size is 1x1x1. It performs a convolutional projection during testing
+    when the input size is larger than 1x1x1.
+
+    Args:
+        dim_in (list): the list of channel dimensions of the p inputs to the
+            ResNetHead.
+        num_classes (int): the channel dimensions of the p outputs to the
+            ResNetHead.
+        pool_size (list): the list of kernel sizes of p spatial temporal
+            poolings, temporal pool kernel size, spatial pool kernel size,
+            spatial pool kernel size in order.
+        dropout_rate (float): dropout rate. If equal to 0.0, perform no
+            dropout.
+        act_func (string): activation function to use. 'softmax': applies
+            softmax on the output. 'sigmoid': applies sigmoid on the output.
+
+    """
+    modules = []
+    if pool_size is None:
+        avg_pool = co.AdaptiveAvgPool3d(
+            (1, 1, 1), kernel_size=temporal_window_size, temporal_fill=temporal_fill
+        )
+    else:
+        avg_pool = co.AvgPool3d(pool_size, stride=1, temporal_fill=temporal_fill)
+
+    modules.append(("avg_pool", avg_pool))
+
+    if dropout_rate > 0.0:
+        modules.append(("dropout", nn.Dropout(dropout_rate)))
+
+    # Perform FC in a fully convolutional manner. The FC layer will be
+    # initialized with a different std comparing to convolutional layers.
+    modules.append(
+        ("projection", co.Linear(dim_in, num_classes, bias=True, channel_dim=2))
+    )
+
+    def not_training(module, *args):
+        return not module.training
+
+    modules.append(
+        (
+            "act",
+            co.Conditional(
+                not_training,
+                {
+                    "softmax": nn.Softmax(dim=1),
+                    "sigmoid": nn.Sigmoid(),
+                }[act_func],
+            ),
+        )
+    )
+
+    def view(x):
+        return x.view(x.shape[0], -1)
+
+    modules.append(("view", co.Lambda(view)))
+
+    return co.Sequential(OrderedDict(modules))
+
+
+def CoResNetRoIHead(
+    dim_in: int,
+    num_classes: int,
+    pool_size: Sequence[int],
+    resolution: Sequence[int],
+    scale_factor: Sequence[int],
+    dropout_rate=0.0,
+    act_func="softmax",
+    aligned=True,
+):
+    """
+    ResNe(X)t RoI head.
+
+    Args:
+        dim_in (list): the list of channel dimensions of the p inputs to the
+            ResNetHead.
+        num_classes (int): the channel dimensions of the p outputs to the
+            ResNetHead.
+        pool_size (list): the list of kernel sizes of p spatial temporal
+            poolings, temporal pool kernel size, spatial pool kernel size,
+            spatial pool kernel size in order.
+        resolution (list): the list of spatial output size from the ROIAlign.
+        scale_factor (list): the list of ratio to the input boxes by this
+            number.
+        dropout_rate (float): dropout rate. If equal to 0.0, perform no
+            dropout.
+        act_func (string): activation function to use. 'softmax': applies
+            softmax on the output. 'sigmoid': applies sigmoid on the output.
+        aligned (bool): if False, use the legacy implementation. If True,
+            align the results more perfectly.
+    Note:
+        Given a continuous coordinate c, its two neighboring pixel indices
+        (in our pixel model) are computed by floor (c - 0.5) and ceil
+        (c - 0.5). For example, c=1.3 has pixel neighbors with discrete
+        indices [0] and [1] (which are sampled from the underlying signal at
+        continuous coordinates 0.5 and 1.5). But the original roi_align
+        (aligned=False) does not subtract the 0.5 when computing neighboring
+        pixel indices and therefore it uses pixels with a slightly incorrect
+        alignment (relative to our pixel model) when performing bilinear
+        interpolation.
+        With `aligned=True`, we first appropriately scale the ROI and then
+        shift it by -0.5 prior to calling roi_align. This produces the
+        correct neighbors; It makes negligible differences to the model's
+        performance if ROIAlign is used together with conv layers.
+    """
+    ...  # TODO: impl
+
+
+def CoResNet(
+    arch: str,
+    dim_in: int,
+    image_size: int,
+    frames_per_clip: int,
+    num_classes: int,
+    resnet_depth: int,
+    resnet_num_groups: int,
+    resnet_width_per_group: int,
+    resnet_dropout_rate: float,
+    resnet_fc_std_init: float,
+    resnet_final_batchnorm_zero_init: bool,
+    resnet_head_act: str = "softmax",
+    enable_detection=False,
+    align_detection=False,
+    temporal_fill: co.PaddingMode = "zeros",
+):
+    """
+    Continual 3D ResNet model (CoC2D, CoI3D, CoSlow),
+    adapted from https://github.com/facebookresearch/SlowFast
+
+    The "Slow" network was originally proposed by
+    Christoph Feichtenhofer, Haoqi Fan, Jitendra Malik, and Kaiming He.
+    "SlowFast networks for video recognition."
+    https://arxiv.org/pdf/1812.03982.pdf
+    """
+    norm_module = nn.BatchNorm3d
+
+    (dep2, dep3, dep4, dep5) = _MODEL_STAGE_DEPTH[resnet_depth]
+    (stride2, stride3, stride4, stride5) = _SPATIAL_STRIDES[enable_detection]
+    (dil2, dil3, dil4, dil5) = _SPATIAL_DILATIONS[enable_detection]
+    pool_size = _POOL1[arch]
+
+    dim_inner = resnet_num_groups * resnet_width_per_group
+
+    temp_kernel = _TEMPORAL_KERNEL_BASIS[arch]
+
+    s1 = CoResNetBasicStem(
+        dim_in=dim_in,
+        dim_out=resnet_width_per_group,
+        kernel=temp_kernel[0][0] + [7, 7],
+        stride=[1, 2, 2],
+        padding=[temp_kernel[0][0][0] // 2, 3, 3],
+        norm_module=norm_module,
+    )
+
+    s2 = CoResStage(
+        dim_in=resnet_width_per_group,
+        dim_out=resnet_width_per_group * 4,
+        dim_inner=dim_inner,
+        temp_kernel_sizes=temp_kernel[1],
+        stride=stride2,
+        num_blocks=dep2,
+        num_groups=resnet_num_groups,
+        num_block_temp_kernel=dep2,
+        trans_func=CoBottleneckTransform,
+        stride_1x1=False,
+        inplace_relu=True,
+        dilation=dil2,
+        norm_module=norm_module,
+    )
+
+    pathway0_pool = co.MaxPool3d(
+        kernel_size=pool_size,
+        stride=pool_size,
+        padding=[0, 0, 0],
+    )
+
+    s3 = CoResStage(
+        dim_in=resnet_width_per_group * 4,
+        dim_out=resnet_width_per_group * 8,
+        dim_inner=dim_inner * 2,
+        temp_kernel_sizes=temp_kernel[2],
+        stride=stride3,
+        num_blocks=dep3,
+        num_groups=resnet_num_groups,
+        num_block_temp_kernel=dep3,
+        trans_func=CoBottleneckTransform,
+        stride_1x1=False,
+        inplace_relu=True,
+        dilation=dil3,
+        norm_module=norm_module,
+    )
+
+    s4 = CoResStage(
+        dim_in=resnet_width_per_group * 8,
+        dim_out=resnet_width_per_group * 16,
+        dim_inner=dim_inner * 4,
+        temp_kernel_sizes=temp_kernel[3],
+        stride=stride4,
+        num_blocks=dep4,
+        num_groups=resnet_num_groups,
+        num_block_temp_kernel=dep4,
+        trans_func=CoBottleneckTransform,
+        stride_1x1=False,
+        inplace_relu=True,
+        dilation=dil4,
+        norm_module=norm_module,
+    )
+
+    s5 = CoResStage(
+        dim_in=resnet_width_per_group * 16,
+        dim_out=resnet_width_per_group * 32,
+        dim_inner=dim_inner * 8,
+        temp_kernel_sizes=temp_kernel[4],
+        stride=stride5,
+        num_blocks=dep5,
+        num_groups=resnet_num_groups,
+        num_block_temp_kernel=dep5,
+        trans_func=CoBottleneckTransform,
+        stride_1x1=False,
+        inplace_relu=True,
+        dilation=dil5,
+        norm_module=norm_module,
+    )
+
+    if enable_detection:
+        head = CoResNetRoIHead(
+            dim_in=resnet_width_per_group * 32,
+            num_classes=num_classes,
+            pool_size=(frames_per_clip // pool_size[0], 1, 1),
+            resolution=[7] * 2,
+            scale_factor=16,
+            dropout_rate=resnet_dropout_rate,
+            act_func=resnet_head_act,
+            aligned=align_detection,
+        )
+    else:
+        head = CoResNetBasicHead(
+            dim_in=resnet_width_per_group * 32,
+            num_classes=num_classes,
+            pool_size=(
+                frames_per_clip // pool_size[0],
+                image_size // 32 // pool_size[1],
+                image_size // 32 // pool_size[2],
+            ),
+            dropout_rate=resnet_dropout_rate,
+            act_func=resnet_head_act,
+            temporal_window_size=frames_per_clip,
+            temporal_fill=temporal_fill,
+        )
+
+    seq = co.Sequential(
+        OrderedDict(
+            [
+                ("s1", s1),
+                ("s2", s2),
+                ("pathway0_pool", pathway0_pool),  # Not actually utilized
+                ("s3", s3),
+                ("s4", s4),
+                ("s5", s5),
+                ("head", head),
+            ]
+        )
+    )
+    init_weights(seq, resnet_fc_std_init, bool(resnet_final_batchnorm_zero_init))
+    return seq
---
+++ models/coresnet/main.py
@@ -0,0 +1,265 @@
+""" CoX3D main """
+from ride import Main  # isort:skip
+from functools import partial
+from typing import Sequence
+
+import torch
+from ride import Configs, RideModule
+from ride.metrics import TopKAccuracyMetric
+from ride.optimizers import SgdOneCycleOptimizer
+from ride.utils.logging import getLogger
+
+from datasets import ActionRecognitionDatasets
+from models.coresnet.coresnet import CoResNet
+
+logger = getLogger("CoResNet")
+
+
+class CoResNetRide(
+    RideModule,
+    ActionRecognitionDatasets,
+    SgdOneCycleOptimizer,
+    TopKAccuracyMetric(1),
+):
+    @staticmethod
+    def configs() -> Configs:
+        c = Configs()
+        c.add(
+            name="resnet_architecture",
+            type=int,
+            default="slow",
+            strategy="constant",
+            choices=["slow", "i3d"],
+            description="Architecture of ResNet.",
+        )
+        c.add(
+            name="resnet_depth",
+            type=int,
+            default=50,
+            strategy="constant",
+            choices=[50, 101],
+            description="Number of layers in ResNet.",
+        )
+        c.add(
+            name="resnet_num_groups",
+            type=int,
+            default=1,
+            strategy="constant",
+            description="Number of groups.",
+        )
+        c.add(
+            name="resnet_width_per_group",
+            type=int,
+            default=64,
+            strategy="constant",
+            description="Width of each group.",
+        )
+        c.add(
+            name="resnet_dropout_rate",
+            type=float,
+            default=0.5,
+            choices=[0.0, 1.0],
+            strategy="uniform",
+            description="Dropout rate before final projection in the backbone.",
+        )
+        c.add(
+            name="resnet_head_activation",
+            type=str,
+            default="softmax",
+            choices=["softmax", "sigmoid"],
+            strategy="choice",
+            description="Activation layer for the output head.",
+        )
+        c.add(
+            name="resnet_fc_std_init",
+            type=float,
+            default=0.01,
+            strategy="choice",
+            description="The std to initialize the fc layer(s).",
+        )
+        c.add(
+            name="resnet_final_batchnorm_zero_init",
+            type=int,
+            default=1,
+            choices=[0, 1],
+            strategy="choice",
+            description="If true, initialize the gamma of the final BN of each block to zero.",
+        )
+        c.add(
+            name="co3d_temporal_fill",
+            type=str,
+            default="zeros",
+            choices=["zeros", "replicate"],
+            strategy="choice",
+            description="Fill mode for samples along temporal dimension. This is used at state initialisation and in `forward_steps` as padding along the temporal dimension.",
+        )
+        c.add(
+            name="co3d_forward_mode",
+            type=str,
+            default="init_frame",
+            choices=["clip", "frame", "init_frame", "init_clip", "clip_init_frame"],
+            strategy="choice",
+            description="Whether to compute clip or frame during forward. If 'clip_init_frame', the network is initialised with a clip and then frame forwards are applied.",
+        )
+        c.add(
+            name="co3d_num_forward_frames",
+            type=int,
+            default=1,
+            description="The number of frames to predict over",
+        )
+        c.add(
+            name="co3d_forward_frame_delay",
+            type=int,
+            default=-1,
+            strategy="choice",
+            description="Number of frames forwards prior to final prediction in 'clip_init_frame' mode. If '-1', a delay of clip_length - 1 is used",
+        )
+        c.add(
+            name="co3d_forward_prediction_delay",
+            type=int,
+            default=0,
+            strategy="choice",
+            description="Number of steps to delay the prediction relative to the frames",
+        )
+        c.add(
+            name="enable_detection",
+            type=int,
+            default=0,
+            strategy="choice",
+            choices=[0, 1],
+            description="Whether to enable detection head.",
+        )
+        c.add(
+            name="align_detection",
+            type=int,
+            default=0,
+            strategy="choice",
+            choices=[0, 1],
+            description="Whether to utilise alignment in detection head.",
+        )
+
+        return c
+
+    def __init__(self, hparams):
+        # Inflate frames_per_clip of dataset, to have some frames for initialization
+        self.temporal_window_size = self.hparams.frames_per_clip
+        image_size = self.hparams.image_size
+        dim_in = 3
+
+        self.module = CoResNet(
+            arch=self.hparams.resnet_architecture,
+            dim_in=dim_in,
+            image_size=image_size,
+            frames_per_clip=self.temporal_window_size,
+            num_classes=self.dataloader.num_classes,  # from ActionRecognitionDatasets,
+            resnet_depth=self.hparams.resnet_depth,
+            resnet_num_groups=self.hparams.resnet_num_groups,
+            resnet_width_per_group=self.hparams.resnet_width_per_group,
+            resnet_dropout_rate=self.hparams.resnet_dropout_rate,
+            resnet_fc_std_init=self.hparams.resnet_fc_std_init,
+            resnet_final_batchnorm_zero_init=self.hparams.resnet_final_batchnorm_zero_init,
+            resnet_head_act=self.hparams.resnet_head_act,
+            enable_detection=False,
+            align_detection=False,
+            temporal_fill=self.hparams.co3d_temporal_fill,
+        )
+        self.module.call_mode = "forward_steps"
+
+        # Ensure that state-dict is flattened
+        self.load_state_dict = partial(self.module.load_state_dict, flatten=True)
+        self.state_dict = partial(self.module.state_dict, flatten=True)
+
+        num_init_frames = max(
+            self.module.receptive_field - 1, self.hparams.co3d_forward_frame_delay - 1
+        )
+        self.hparams.frames_per_clip = (
+            num_init_frames
+            + self.hparams.co3d_num_forward_frames
+            + self.hparams.co3d_forward_prediction_delay
+        )
+
+        # From ActionRecognitionDatasets
+        frames_per_clip = (
+            1
+            if self.hparams.co3d_forward_mode == "frame"
+            else self.hparams.frames_per_clip
+        )
+        self.input_shape = (dim_in, frames_per_clip, image_size, image_size)
+
+        # Assuming Conv3d have stride = 1 and dilation = 1, and that no other modules delay the network.
+        logger.info(f"Model receptive field: {self.module.receptive_field} frames")
+
+    def preprocess_batch(self, batch):
+        """Overloads method in ride.Lifecycle"""
+
+        x, y = batch[0], batch[1]
+        x = x[:, :, : -self.hparams.co3d_forward_prediction_delay or None]
+
+        # Ensure that there are enough frames
+        num_init_frames = max(
+            self.module.receptive_field - 1, self.hparams.co3d_forward_frame_delay - 1
+        )
+        num_needed_frames = num_init_frames + self.hparams.co3d_num_forward_frames
+        num_missing_frames = num_needed_frames - x.shape[2]
+        if num_missing_frames > 0:
+            # Repeat the last frame
+            append = (
+                x[:, :, -1]
+                .repeat((num_missing_frames, 1, 1, 1, 1))
+                .permute(1, 2, 0, 3, 4)
+            )
+            x = torch.cat([x, append], dim=2)
+            assert x.shape[2] == num_needed_frames
+
+        if self.task == "detection":
+            y = y[:, self.hparams.co3d_forward_prediction_delay :]
+            if num_missing_frames > 0:
+                append = y[:, -1].repeat((num_missing_frames, 1)).permute(1, 0)
+                y = torch.cat([y, append], dim=1)
+
+            # Remove labels for initialisation frames
+            y = y[:, -self.hparams.co3d_num_forward_frames :]
+
+            # Collapse into batch dimension
+            y = y.reshape(-1)
+
+        batch = [x, y]
+        return batch
+
+    def clean_state_on_shape_change(self, shape):
+        if getattr(self, "_current_input_shape", None) != shape:
+            self.module.clean_state()
+            self._current_input_shape = shape
+
+    def forward(self, x):
+        if not hasattr(self.hparams, "profile"):
+            self.module.clean_state()
+
+        result = None
+
+        if "init" in self.hparams.co3d_forward_mode:
+            self.warm_up(tuple(x[:, :, 0].shape))
+
+        num_init_frames = max(
+            self.module.receptive_field - 1, self.hparams.co3d_forward_frame_delay - 1
+        )
+        self.module(x[:, :, :num_init_frames])  # = forward_steps don't save
+        result = self.module(x[:, :, num_init_frames:])  # = forward_steps
+
+        if self.task == "classification":
+            result = result.mean(dim=-1)
+        elif self.task == "detection":
+            result = result.permute(0, 2, 1).reshape(-1, self.num_classes)
+        else:
+            raise ValueError(f"Unknown task {self.task}")
+
+        return result
+
+    def warm_up(self, input_shape: Sequence[int], *args, **kwargs):
+        for m in self.module.modules():
+            if hasattr(m, "state_index"):
+                m.state_index = 0
+
+
+if __name__ == "__main__":
+    Main(CoResNetRide).argparse()
