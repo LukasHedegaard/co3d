cd424dd0d548f2db903e7b591ab55aeb34015b18
LukasHedegaard <lh@eng.au.dk>
2021-11-04 13:22:30 +0000

    Add slow kinetics test script

--- models/coresnet/scripts/test/kinetics400.sh
+++ models/coresnet/scripts/test/kinetics400.sh
@@ -6,32 +6,60 @@ fi
 
 PROJECT=models/coresnet
 DATASET=kinetics400
-GPUS=1
+GPUS=7
 DISTRIBUTED_BACKEND=ddp
-PRECISION=32
+PRECISION=16
 
 # Run test sequence ######################
-# Allow for repetition of last frame up to 70% of steady-state (heuristic choice)
-
-FRAMES_PER_CLIP=64
-FORWARD_FRAME_DELAY=124 # (114 + 64) * 0.7
 
 python $PROJECT/main.py \
-    --id CoSlow_kin \
+    --id CoSlow_kin_clip \
     --dataset $DATASET \
     --seed 42 \
     --gpus $GPUS \
     --from_hparams_file models/coresnet/hparams/slow_8x8_kinetics.yaml \
     --finetune_from_weights models/coresnet/weights/slow_8x8_kinetics.pyth \
-    --batch_size 4 \
-    --co3d_forward_mode init_frame \
-    --co3d_temporal_fill replicate \
-    --co3d_num_forward_frames 1 \
-    --co3d_forward_frame_delay $FORWARD_FRAME_DELAY  \
+    --batch_size 8 \
     --benchmark True \
     --test \
     --logging_backend wandb \
     --num_workers 4 \
-    --frames_per_clip $FRAMES_PER_CLIP \
     --precision $PRECISION \
     --distributed_backend $DISTRIBUTED_BACKEND \
+    --co3d_forward_mode clip \
+
+
+python $PROJECT/main.py \
+    --id CoSlow_kin_frames_26 \
+    --dataset $DATASET \
+    --seed 42 \
+    --gpus $GPUS \
+    --from_hparams_file models/coresnet/hparams/slow_8x8_kinetics.yaml \
+    --finetune_from_weights models/coresnet/weights/slow_8x8_kinetics.pyth \
+    --batch_size 8 \
+    --benchmark True \
+    --test \
+    --logging_backend wandb \
+    --num_workers 4 \
+    --precision $PRECISION \
+    --distributed_backend $DISTRIBUTED_BACKEND \
+    --co3d_forward_mode init_frame \
+  
+
+python $PROJECT/main.py \
+    --id CoSlow_kin_frames_64 \
+    --dataset $DATASET \
+    --seed 42 \
+    --gpus $GPUS \
+    --from_hparams_file models/coresnet/hparams/slow_8x8_kinetics.yaml \
+    --finetune_from_weights models/coresnet/weights/slow_8x8_kinetics.pyth \
+    --batch_size 8 \
+    --benchmark True \
+    --test \
+    --logging_backend wandb \
+    --num_workers 4 \
+    --precision $PRECISION \
+    --distributed_backend $DISTRIBUTED_BACKEND \
+    --frames_per_clip 64 \
+    --co3d_forward_mode init_frame \
+
