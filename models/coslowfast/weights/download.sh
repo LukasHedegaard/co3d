a71d949ef478b9474ad3fd0720877332f8368f20
LukasHedegaard <lh@eng.au.dk>
2021-11-08 16:19:38 +0000

    Add script for downloading slowfast weights

---
+++ models/coslowfast/weights/download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://dl.fbaipublicfiles.com/pytorchvideo/model_zoo/kinetics/SLOWFAST_8x8_R50.pyth -O slowfast_8x8_kinetics.pyth
+
+wget https://dl.fbaipublicfiles.com/pytorchvideo/model_zoo/charades/SLOWFAST_8x8_R50.pyth -O slowfast_8x8_charades.pyth
+
+wget https://dl.fbaipublicfiles.com/pytorchvideo/model_zoo/ava/SLOWFAST_8x8_R50_DETECTION.pyth -O slowfast_8x8_ava.pyth
