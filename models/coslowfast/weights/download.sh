30ff99122f5a122980f677f950967abbd12b7798
LukasHedegaard <lh@eng.au.dk>
2021-11-16 20:54:09 +0000

    Update coslow weight download

--- models/coslowfast/weights/download.sh
+++ models/coslowfast/weights/download.sh
@@ -5,3 +5,5 @@ wget https://dl.fbaipublicfiles.com/pytorchvideo/model_zoo/kinetics/SLOWFAST_8x8
 wget https://dl.fbaipublicfiles.com/pytorchvideo/model_zoo/charades/SLOWFAST_8x8_R50.pyth -O slowfast_8x8_charades.pyth
 
 wget https://dl.fbaipublicfiles.com/pytorchvideo/model_zoo/ava/SLOWFAST_8x8_R50_DETECTION.pyth -O slowfast_8x8_ava.pyth
+
+wget https://dl.fbaipublicfiles.com/pytorchvideo/model_zoo/ssv2/SLOWFAST_8x8_R50.pyth -O slowfast_8x8_ssv2.pyth
