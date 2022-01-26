ef61764e4ac2506e89e94e173a097cd26b5e62c0
LukasHedegaard <lh@eng.au.dk>
2022-01-26 14:28:23 +0000

    Update tvseries train script

--- models/cox3d/scripts/train/tvseries.sh
+++ models/cox3d/scripts/train/tvseries.sh
@@ -6,35 +6,33 @@ fi
 
 PROJECT=models/cox3d
 DATASET=tvseries
-GPUS=2
-PRECISION=16
+GPUS=1
+PRECISION=32
 
 ## Run train sequence
 
 MODEL=s
-FRAMES_PER_CLIP=64
-FORWARD_FRAME_DELAY=64
-NUM_FORWARD_FRAMES=64
-LR=0.3
+FRAMES_PER_CLIP=13
+NUM_FORWARD_FRAMES=8
+LR=0.1
 
 python $PROJECT/main.py \
     --id "CoX3D_${MODEL}_${DATASET}_${FRAMES_PER_CLIP}_frames" \
     --dataset $DATASET \
+    --metric_selection $DATASET \
     --seed 42 \
     --gpus $GPUS \
     --from_hparams_file models/x3d/hparams/$MODEL.yaml \
     --finetune_from_weights models/x3d/weights/x3d_$MODEL.pyth \
     --batch_size 16 \
-    --accumulate_grad_batches 2 \
     --train \
     --max_epochs 30 \
     --logging_backend wandb \
     --num_workers 8 \
     --co3d_forward_mode init_frame \
-    --co3d_forward_frame_delay $FORWARD_FRAME_DELAY  \
     --co3d_num_forward_frames $NUM_FORWARD_FRAMES \
     --step_between_clips $NUM_FORWARD_FRAMES \
-    --temporal_window_size $TEMPORAL_WINDOW_SIZE \
+    --frames_per_clip $FRAMES_PER_CLIP \
     --precision $PRECISION \
     --optimization_metric mcAP \
     --learning_rate $LR \
@@ -42,14 +40,10 @@ python $PROJECT/main.py \
     --x3d_dropout_rate 0.5 \
     --weight_decay 0.0006 \
     --unfreeze_from_epoch 0 \
+    --unfreeze_layer_step 1 \
     --unfreeze_epoch_step 100 \
-    --mean_average_precision_skip_classes 0,21 \
+    --mean_average_precision_skip_classes 0 \
     --log_every_n_steps 1 \
-    --dataloader_prefetch_factor 4 \
     --rand_augment_magnitude 11 \
     --rand_augment_num_layers 4 \
-    --distributed_backend ddp \
-    # --unfreeze_layer_step 2 \
-    # --benchmark True \
-    # --discriminative_lr_fraction 0.001 \
 
