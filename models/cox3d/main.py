00522fa678bd8bb1df24549ef01873d855285054
LukasHedegaard <lh@eng.au.dk>
2022-01-26 14:28:11 +0000

    Update cox3d to use common base

--- models/cox3d/main.py
+++ models/cox3d/main.py
@@ -1,28 +1,29 @@
 """ CoX3D main """
 from ride import Main  # isort:skip
-from functools import partial
-from typing import Sequence
-
-import torch
 from ride import Configs, RideModule
-from ride.metrics import TopKAccuracyMetric
+from ride.metrics import TopKAccuracyMetric, MetricSelector
 from ride.optimizers import SgdOneCycleOptimizer
 from ride.utils.logging import getLogger
 
+from functools import partial
 from datasets import ActionRecognitionDatasets
-from losses import LabelSmoothingCrossEntropy
 from metrics import CalibratedMeanAveragePrecisionMetric
 from models.cox3d.modules.x3d import CoX3D
+from models.common import Co3dBase
 
 logger = getLogger("CoX3D")
 
 
 class CoX3DRide(
     RideModule,
+    Co3dBase,
     ActionRecognitionDatasets,
     SgdOneCycleOptimizer,
-    TopKAccuracyMetric(1),
-    CalibratedMeanAveragePrecisionMetric,
+    MetricSelector(
+        kinetics400=TopKAccuracyMetric(1),
+        tvseries=CalibratedMeanAveragePrecisionMetric,
+        default_config="kinetics400",
+    ),
 ):
     @staticmethod
     def configs() -> Configs:
@@ -123,61 +124,12 @@ class CoX3DRide(
             strategy="choice",
             description="If true, initialize the gamma of the final BN of each block to zero.",
         )
-        c.add(
-            name="co3d_temporal_fill",
-            type=str,
-            default="zeros",
-            choices=["zeros", "replicate"],
-            strategy="choice",
-            description="Fill mode for samples along temporal dimension. This is used at state initialisation and in `forward_steps` as padding along the temporal dimension.",
-        )
-        c.add(
-            name="co3d_forward_mode",
-            type=str,
-            default="init_frame",
-            choices=["clip", "frame", "init_frame", "init_clip", "clip_init_frame"],
-            strategy="choice",
-            description="Whether to compute clip or frame during forward. If 'clip_init_frame', the network is initialised with a clip and then frame forwards are applied.",
-        )
-        c.add(
-            name="co3d_num_forward_frames",
-            type=int,
-            default=1,
-            description="The number of frames to predict over",
-        )
-        c.add(
-            name="co3d_forward_frame_delay",
-            type=int,
-            default=-1,
-            strategy="choice",
-            description="Number of frames forwards prior to final prediction in 'clip_init_frame' mode. If '-1', a delay of clip_length - 1 is used",
-        )
-        c.add(
-            name="co3d_forward_prediction_delay",
-            type=int,
-            default=0,
-            strategy="choice",
-            description="Number of steps to delay the prediction relative to the frames",
-        )
-        c.add(
-            name="temporal_window_size",
-            type=int,
-            default=64,
-            strategy="choice",
-            description="Temporal window size for global average pool.",
-        )
-
         return c
 
     def __init__(self, hparams):
-        # Inflate frames_per_clip of dataset, to have some frames for initialization
-        image_size = self.hparams.image_size
-        dim_in = 3
-        self.hparams.frames_per_clip = self.hparams.temporal_window_size
-
         self.module = CoX3D(
-            dim_in,
-            image_size,
+            self.dim_in,
+            self.hparams.image_size,
             self.hparams.temporal_window_size,
             self.dataloader.num_classes,  # from ActionRecognitionDatasets
             self.hparams.x3d_conv1_dim,
@@ -196,116 +148,10 @@ class CoX3DRide(
             self.hparams.co3d_temporal_fill,
             se_scope="frame",
         )
-        if "frame" in self.hparams.co3d_forward_mode:
-            self.module.call_mode = "forward_steps"
 
         # Ensure that state-dict is flattened
         self.load_state_dict = partial(self.module.load_state_dict, flatten=True)
         self.state_dict = partial(self.module.state_dict, flatten=True)
-
-        num_init_frames = max(
-            self.module.receptive_field - 1, self.hparams.co3d_forward_frame_delay - 1
-        )
-        self.hparams.frames_per_clip = (
-            num_init_frames
-            + self.hparams.co3d_num_forward_frames
-            + self.hparams.co3d_forward_prediction_delay
-        )
-
-        # From ActionRecognitionDatasets
-        frames_per_clip = (
-            1
-            if self.hparams.co3d_forward_mode == "frame"
-            else self.hparams.frames_per_clip
-        )
-        self.input_shape = (dim_in, frames_per_clip, image_size, image_size)
-
-        # Assuming Conv3d have stride = 1 and dilation = 1, and that no other modules delay the network.
-        logger.info(f"Model receptive field: {self.module.receptive_field} frames")
-
-        if self.hparams.dataset == "thumos14":
-            class_counts = torch.tensor(
-                self.dataloader.test_dataloader.dataset.class_counts, dtype=torch.float
-            )
-            class_weights = class_counts.sum() / len(class_counts) / class_counts
-            class_weights[class_counts == 0] = 1.0
-            self.loss = torch.nn.CrossEntropyLoss(class_weights, ignore_index=21)
-            # self.loss = LabelSmoothingCrossEntropy(smoothing=0.1, ignore_classes=21)
-            self.hparams.mean_average_precision_skip_classes = [0, 21]
-        elif self.hparams.dataset == "tvseries":
-            self.loss = LabelSmoothingCrossEntropy(smoothing=0.1)
-            self.hparams.mean_average_precision_skip_classes = [0]
-
-    def preprocess_batch(self, batch):
-        """Overloads method in ride.Lifecycle"""
-
-        x, y = batch[0], batch[1]
-        x = x[:, :, : -self.hparams.co3d_forward_prediction_delay or None]
-
-        # Ensure that there are enough frames
-        num_init_frames = max(
-            self.module.receptive_field - 1, self.hparams.co3d_forward_frame_delay - 1
-        )
-        num_needed_frames = num_init_frames + self.hparams.co3d_num_forward_frames
-        num_missing_frames = num_needed_frames - x.shape[2]
-        if num_missing_frames > 0:
-            # Repeat the last frame
-            append = (
-                x[:, :, -1]
-                .repeat((num_missing_frames, 1, 1, 1, 1))
-                .permute(1, 2, 0, 3, 4)
-            )
-            x = torch.cat([x, append], dim=2)
-            assert x.shape[2] == num_needed_frames
-
-        if self.task == "detection":
-            y = y[:, self.hparams.co3d_forward_prediction_delay :]
-            if num_missing_frames > 0:
-                append = y[:, -1].repeat((num_missing_frames, 1)).permute(1, 0)
-                y = torch.cat([y, append], dim=1)
-
-            # Remove labels for initialisation frames
-            y = y[:, -self.hparams.co3d_num_forward_frames :]
-
-            # Collapse into batch dimension
-            y = y.reshape(-1)
-
-        batch = [x, y]
-        return batch
-
-    def clean_state_on_shape_change(self, shape):
-        if getattr(self, "_current_input_shape", None) != shape:
-            self.module.clean_state()
-            self._current_input_shape = shape
-
-    def forward(self, x):
-        if not hasattr(self.hparams, "profile"):
-            self.module.clean_state()
-
-        result = None
-
-        if "init" in self.hparams.co3d_forward_mode:
-            self.warm_up(tuple(x[:, :, 0].shape))
-
-        num_init_frames = max(
-            self.module.receptive_field - 1, self.hparams.co3d_forward_frame_delay - 1
-        )
-        self.module(x[:, :, :num_init_frames])  # = forward_steps don't save
-        result = self.module(x[:, :, num_init_frames:])  # = forward_steps
-
-        if self.task == "classification":
-            result = result.mean(dim=-1)
-        elif self.task == "detection":
-            result = result.permute(0, 2, 1).reshape(-1, self.num_classes)
-        else:
-            raise ValueError(f"Unknown task {self.task}")
-
-        return result
-
-    def warm_up(self, input_shape: Sequence[int], *args, **kwargs):
-        for m in self.module.modules():
-            if hasattr(m, "state_index"):
-                m.state_index = 0
 
 
 if __name__ == "__main__":
