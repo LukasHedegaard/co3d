9efd3bd153b89e754496dc5c3a61d25af1e4df65
LukasHedegaard <lh@eng.au.dk>
2022-07-05 11:06:06 +0200

    Change missing ffmpeg from assertion to error log

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,5 +1,7 @@
 import os
 
+from ride.utils.logging import getLogger
+
 from .dataloader import ActionRecognitionDatasets  # noqa: F401
 from .interfaces import VideoClassificationDataset  # noqa: F401
 from .video_ensemble import (  # noqa: F401
@@ -8,8 +10,11 @@ from .video_ensemble import (  # noqa: F401
 )
 from .videoclips import videoclips  # noqa: F401
 
+logger = getLogger("datasets")
+
 # Ensure ffmpeg is accessible
 os.environ["PATH"] += os.pathsep + "/usr/local/bin"
 os.environ["PATH"] += os.pathsep + "/usr/bin"
 ffmpeg_path = os.popen("which ffmpeg").read()[:-1]
-assert ffmpeg_path, "No ffmpeg installation found"
+if not ffmpeg_path:
+    logger.error("No ffmpeg installation found")
